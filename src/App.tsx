eac323418c4ea67a48bc8da899578a46ee5127f7
Lucas Lima <lucaslbl@outlook.com>
2021-05-14 16:14:24 -0300

    created modal component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement("#root");
 
@@ -23,14 +24,12 @@ export function App() {
 
       <Dashboard />
 
-      <GlobalStyle />
-
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
+
+      <GlobalStyle />
     </>
   );
 }
